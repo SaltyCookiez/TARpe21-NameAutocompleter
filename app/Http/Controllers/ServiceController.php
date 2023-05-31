ee8e9edd05387d653c747376c09dfe0e3e7d5117
Kristjan Kivikangur <kristjan.kivikangur@tthk.ee>
2023-05-31 12:52:57 +0300

    Enable editing of services

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -15,7 +15,7 @@ class ServiceController extends Controller
      */
     public function index() : View
     {
-        return View("service.index",[
+        return View("services.index",[
             'services'=>Service::all(),
         ]);
     }
@@ -57,17 +57,35 @@ class ServiceController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Service $service)
+    public function edit(Service $service): View
     {
-        //
+        //TODO Authorization
+        //$this->authorize('update',$service);
+
+        return view('services.edit',[
+            'service' => $service,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Service $service)
+    public function update(Request $request, Service $service):RedirectResponse
     {
-        //
+        //TODO Authorization - https://bootcamp.laravel.com/blade/editing-chirps#authorization
+        //$this->authorize('update', $chirp);
+
+        //TODO move validation logic so not duplicated - https://laravel.com/docs/validation#form-request-validation
+        $validated = $request->validate([
+            'name' => 'required|string|max:128',
+            'basePrice_cents' => 'integer|gte:0',
+            'duration_minutes' => 'integer|gte:0',
+            'description' => 'nullable|string',
+        ]);
+
+        $service->update($validated);
+
+        return redirect(route('services.index'));
     }
 
     /**
